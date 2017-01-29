bab73cd0626ce71bf76836432f12da0a0a38e1a2
Robin McCorkell <robin@mccorkell.me.uk>
2017-01-29 02:37:57 +0000

    Add EntityTemplates

---
+++ workers/unity/Assets/EntityTemplates/EnvironmentNodeEntityTemplate.cs
@@ -0,0 +1,27 @@
+﻿using Improbable.Unity.Core.Acls;
+using Evolution.Environment;
+using Improbable.Collections;
+using UnityEngine;
+
+namespace Assets.EntityTemplates
+{
+    public class EnvironmentNodeEntityTemplate : MonoBehaviour
+    {
+        public static Entity GenerateEnvironmentNodeEntityTemplate(Coordinates position, Map<MaterialType, uint> initialResources)
+        {
+            var entity = new Entity();
+
+            entity.Add(new Position.Data(new PositionData(position)));
+            entity.Add(new Environment.Data(new EnvironmentData(initalResources)));
+
+            var acl = Acl.Build()
+                .SetReadAccess(CommonPredicates.PhysicsOrVisual)
+                .SetWriteAccess<Position>(CommonPredicates.PhysicsOnly)
+                .SetWriteAccess<Environment>(CommonPredicates.PhysicsOrVisual);
+
+            entity.SetAcl(acl);
+
+            return entity;
+        }
+    }
+}
---
+++ workers/unity/Assets/EntityTemplates/OrganismEntityTemplate.cs
@@ -0,0 +1,36 @@
+﻿using Improbable.Unity.Core.Acls;
+using Evolution.Organism;
+using Evolution.Environment;
+using Improbable.Collections;
+using UnityEngine;
+
+namespace Assets.EntityTemplates
+{
+    public class OrganismEntityTemplate : MonoBehaviour
+    {
+        public static Entity GenerateOrganismEntityTemplate(Coordinates position, Genome genome)
+        {
+            var entity = new Entity();
+
+            entity.Add(new Consumer.Data(
+                        new ConsumerData(MaterialType.A, MaterialType.B, 1.0)
+                        ));
+            entity.Add(new Reproducer.Data(new ReproducerData(genome, genome)));
+            entity.Add(new Mover.Data(new MoverData(position, 1.0, 1)));
+            entity.Add(new Affectable.Data(
+                        new AffectableData(new Map<MaterialType, uint>())
+                        ));
+
+            var acl = Acl.Build()
+                .SetReadAccess(CommonPredicates.PhysicsOrVisual)
+                .SetWriteAccess<Consumer>(CommonPredicates.PhysicsOnly)
+                .SetWriteAccess<Reproducer>(CommonPredicates.PhysicsOnly)
+                .SetWriteAccess<Mover>(CommonPredicates.PhysicsOnly)
+                .SetWriteAccess<Affectable>(CommonPredicates.PhysicsOnly);
+
+            entity.SetAcl(acl);
+
+            return entity;
+        }
+    }
+}
